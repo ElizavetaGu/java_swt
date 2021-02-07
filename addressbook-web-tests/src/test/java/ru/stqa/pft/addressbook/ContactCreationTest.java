7405b91f6f53e5470b3ed0c678216871182e36e1
Liza G <gurovaliz@gmail.com>
2021-02-07 14:02:31 +0300

    добавлены TestBase, GroupDeletion

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactCreationTest.java
@@ -5,26 +5,8 @@ import org.testng.annotations.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class ContactCreationTest {
+public class ContactCreationTest extends TestBase {
   private WebDriver wd;
-
-  @BeforeMethod(alwaysRun = true)
-  public void setUp() throws Exception {
-    wd = new FirefoxDriver();
-    wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-    wd.get("http://localhost/addressbook/");
-    login("admin", "secret");
-  }
-
-  private void login(String username, String password) {
-    wd.findElement(By.name("user")).click();
-    wd.findElement(By.name("user")).clear();
-    wd.findElement(By.name("user")).sendKeys(username);
-    wd.findElement(By.name("pass")).click();
-    wd.findElement(By.name("pass")).clear();
-    wd.findElement(By.name("pass")).sendKeys(password);
-    wd.findElement(By.xpath("//input[@value='Login']")).click();
-    }
 
   @Test
   public void testContactCreation() throws Exception {
@@ -35,9 +17,7 @@ public class ContactCreationTest {
     logout();
   }
 
-  private void logout() {
-    wd.findElement(By.linkText("Logout")).click();
-  }
+
 
   private void returnToContactPage() {
     wd.findElement(By.linkText("home page")).click();
