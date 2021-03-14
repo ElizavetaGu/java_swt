9fbaddd25bc3286f7bec660034c9ee6ffe193623
Liza Gurova <gurovaliz@gmail.com>
2021-03-14 19:50:18 +0300

    Лекции 6.1-6.3 GroupDataGenerator

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -4,6 +4,8 @@ import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -13,8 +15,9 @@ public class ContactCreationTest extends TestBase {
   public void testContactCreation() throws Exception {
     app.goTo().homePage();
     Contacts before = app.contact().all();
+    File photo = new File("src/test/resources/tiare.jpg"); //относительный путь
     ContactData contact = new ContactData().withFirstName("Liza").withLastName("Gurova").withAddress("Street")
-            .withEmail("12@er.ry").withMobilePhone("1234").withGroup("test14");
+            .withEmail("12@er.ry").withMobilePhone("1234").withGroup("test14").withPhoto(photo);
     app.contact().create(contact, true);
     Contacts after = app.contact().all();
     assertThat(after.size(), equalTo(before.size() + 1));
@@ -22,6 +25,15 @@ public class ContactCreationTest extends TestBase {
     //анонимная функция, которая принимает контакт в качестве параметра и возвращает индентификатор контакта
      assertThat(after, equalTo(
              before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
+  }
+
+  @Test (enabled = false)
+  public void testCurrentDir(){
+    File currentDir = new File(".");
+    System.out.println(currentDir.getAbsoluteFile());
+    File photo = new File("src/test/resources/tiare.jpg");
+    System.out.println(photo.getAbsoluteFile());
+    System.out.println(photo.exists());
   }
 
 }
